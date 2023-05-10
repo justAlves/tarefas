f811769eb70e9bc7355ef6f26041de1f824ecc5e
justAlves <iamalves.jpg@gmail.com>
2023-05-10 12:04:34 -0300

    Instalado o firebase

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAyWEnlAba3Tv7VtkJbufTE4CEHjUlOVE8",
+    authDomain: "tarefas-41881.firebaseapp.com",
+    projectId: "tarefas-41881",
+    storageBucket: "tarefas-41881.appspot.com",
+    messagingSenderId: "321165491880",
+    appId: "1:321165491880:web:c696dd9c9153c0c8ec4c9d"
+};
+
+if(!firebase.getApps().length){
+    firebase.initializeApp(firebaseConfig)
+}
+
+export default firebase;
